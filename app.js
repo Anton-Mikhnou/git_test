ef724b0e19c4d701c3c55733a6e54a11d34fb49f
a.mihnov <a.mihnov@anlax.org>
2023-11-28 21:49:11 +0300

    try to do new arr

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 function findMissingLetter(array) {
-  let favoriteArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
-  'p','q','r','s','t','u','v','w','x','y','z'];
-  let rightArr = favoriteArr.slice().filter(item => favoriteArr.includes(item) && array.includes(item))
-  for (let i = 0 ; i < rightArr.length ; i++) {
-    for (let j = 0 ; j < array.length; j++) {
-      if (!array[j].includes(rightArr[i])) {
-        return rightArr[i];        
-      }
+    let favoriteArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
+    'p','q','r','s','t','u','v','w','x','y','z'];
+    let rightArr = favoriteArr.slice().filter(item => favoriteArr.includes(item) && array.includes(item))
+    for (let i = 0; i < array.length; i++){
+        if(rightArr.includes(array[i]) == false){
+            return array[i];
+        }
     }
-  }
 }
 
 console.log(findMissingLetter(['a','b','c','d','f']));
